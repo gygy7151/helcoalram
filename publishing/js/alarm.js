3b754d01550816761ef1dfc9b3bf1ccffcaad69a
kimseongtae <rlatjdxo4907@naver.com>
2021-08-16 20:17:09 +0900

    210816_기능 3-2 오디오 추가

--- publishing/js/alarm.js
+++ publishing/js/alarm.js
@@ -1,6 +1,6 @@
 const warning = confirm('알람이 울립니다. 미션을 진행해 주세요.')
 const audio = new Audio();
-audio.src = "./audio/긴 전화벨 소리.MP3";
+audio.src = "audio/긴 전화벨 소리.MP3";
 audio.autoplay = true;
 audio.loop = true;
 
